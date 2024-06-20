580f7953b5567a1f7b5455f26746bed00814372c
Delgorithm <delgorithm@gmail.com>
2024-06-21 01:52:57 +0200

    Feature : Section social proof added

---
+++ src/components/Newsletter.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <section class="bg-[#2e8b57]"></section>
+</template>
--- src/components/SocialProofs.vue
+++ src/components/SocialProofs.vue
@@ -1,6 +1,10 @@
 <script setup>
+import { ref } from 'vue'
 import socialProof from '../assets/data/socialProof.json'
-console.log(socialProof)
+
+const firstState = ref(true)
+const secondState = ref(false)
+const thirdState = ref(false)
 </script>
 
 <template>
@@ -15,5 +19,86 @@ console.log(socialProof)
     <p class="py-4 font-extralight text-sm leading-6">
       {{ socialProof.blogPart[0].secondParagraph }}
     </p>
+  </section>
+  <section class="p-10">
+    <h2 class="text-2xl font-semibold mx-2 text-center">
+      <span class="text-[#2e8b57]">Les mots</span> de nos voyageurs 😍
+    </h2>
+    <article>
+      <div v-if="firstState">
+        <p class="px-5 py-10 text-center">
+          {{ socialProof.quoteTravelers[0].firstTraveler.firstQuote }}
+        </p>
+        <div class="flex justify-around gap-5">
+          <img
+            src="../assets/Img/OOzgsubRRRiMprrkTtng-min.jpeg"
+            alt="Berger dans les Pyrénées"
+            class="w-32 h-36 rounded-[3rem] p-1 bg-pink-200"
+          />
+          <div>
+            <p class="text-xs">{{ socialProof.quoteTravelers[0].firstTraveler.details }}</p>
+            <p class="font-semibold tracking-wide">
+              {{ socialProof.quoteTravelers[0].firstTraveler.secondQuote }}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div v-if="secondState">
+        <p class="px-5 py-10 text-center">
+          {{ socialProof.quoteTravelers[0].secondTraveler.firstQuote }}
+        </p>
+        <div class="flex justify-around gap-5">
+          <img
+            src="../assets/Img/EGNwnNRnTMCEuTq6bj45-min.jpeg"
+            alt="Berger dans les Pyrénées"
+            class="w-32 h-36 rounded-[3rem] p-1 bg-pink-200"
+          />
+          <div>
+            <p class="text-xs">{{ socialProof.quoteTravelers[0].secondTraveler.details }}</p>
+            <p class="font-semibold tracking-wide">
+              {{ socialProof.quoteTravelers[0].secondTraveler.secondQuote }}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div v-if="thirdState">
+        <p class="px-5 py-10 text-center">
+          {{ socialProof.quoteTravelers[0].firstTraveler.firstQuote }}
+        </p>
+        <div class="flex justify-around gap-5">
+          <img
+            src="../assets/Img/aUfx5rXLQqiRHbypIpXN-min.jpeg"
+            alt="Berger dans les Pyrénées"
+            class="w-32 h-36 rounded-[3rem] p-1 bg-pink-200"
+          />
+          <div>
+            <p class="text-xs">{{ socialProof.quoteTravelers[0].thirdTraveler.details }}</p>
+            <p class="font-semibold tracking-wide">
+              {{ socialProof.quoteTravelers[0].thirdTraveler.secondQuote }}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-between pt-10">
+        <img
+          src="../assets/Img/hI3cO2QQYiahepsX3hwa-min.jpeg"
+          alt="First traveller"
+          class="size-16 rounded-full"
+          @click="firstState = !firstState"
+        />
+        <img
+          src="../assets/Img/PuvPCEcEQvaKNOaJNxHY-min.jpeg"
+          alt="Second traveller"
+          class="size-16 rounded-full"
+          @click="secondState = !secondState"
+        />
+        <img
+          src="../assets/Img/XWtj7pZvTa6Gfwq1uGGn-min.jpeg"
+          alt="Third traveller"
+          class="size-16 rounded-full"
+          @click="thirdState = !thirdState"
+        />
+      </div>
+    </article>
   </section>
 </template>
