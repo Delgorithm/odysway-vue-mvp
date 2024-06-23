0b74ebe17487d71b45f3d56388fa46d469f098dd
Delgorithm <delgorithm@gmail.com>
2024-06-23 16:36:25 +0200

    Minor fixes

--- src/views/ArticleDetails.vue
+++ src/views/ArticleDetails.vue
@@ -1,5 +1,5 @@
 <script setup></script>
 
 <template>
-  <h1 class="h-screen items-center text-center">Details de l'article {{ route.params.id }}</h1>
+  <h1 class="h-screen items-center text-center">Details de l'article {{ $route.params.id }}</h1>
 </template>
