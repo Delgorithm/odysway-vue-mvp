992bf93d75afd77e69f18e4832543ee75c8774c1
Delgorithm <delgorithm@gmail.com>
2024-06-23 16:34:37 +0200

    Feature : Vue router added

--- src/router/index.js
+++ src/router/index.js
@@ -2,31 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import Article from '@/views/Article.vue'
 import ArticleDetails from '@/views/ArticleDetails.vue'
-import Registration from '@/views/Registration.vue'
+
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/article', component: Article },
+  { path: '/article/:id', component: ArticleDetails }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      children: [
-        {
-          path: '/article',
-          component: Article
-        },
-        {
-          path: '/article/:id',
-          component: ArticleDetails
-        },
-        {
-          path: '/registration',
-          component: Registration
-        }
-      ]
-    }
-  ]
+  history: createWebHistory(),
+  routes
 })
 
 export default router
