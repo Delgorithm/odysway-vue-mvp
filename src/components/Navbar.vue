709eb85c414c1e918a146e6ee31246439c859620
Delgorithm <delgorithm@gmail.com>
2024-06-23 14:32:07 +0200

    Fix : Animation + Style added to the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,54 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 const displayModal = ref(false)
 const displayMenu = ref(false)
+const isFixed = ref(false)
+
+function handleScroll() {
+  isFixed.value = window.scrollY > 0
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
 </script>
 
 <template>
-  <nav class="flex justify-between items-center p-2 mx-4">
-    <a href="/">
-      <img src="../assets/Img/logo_couleur-min.png" alt="Odysway's logo" class="w-32" />
-    </a>
-    <section class="flex gap-6">
-      <img
-        src="../assets/Img/bf25558b-2a5d-43a2-9e75-2eff32be2ce8.svg"
-        alt="Searchbar"
-        class="w-6 hover:rounded-xl hover:bg-gray-100"
-        @click="displayModal = !displayModal"
-      />
-
-      <a href="/registration" class="w-6 hover:rounded-xl hover:bg-gray-100">
-        <img src="../assets/Img/048756c9-5947-4b56-8420-5d31d0957cfb.svg" alt="My account" />
+  <header
+    :class="{
+      'bg-white scale-105': isFixed,
+      'bg-[#2e8b57] transition-all duration-500 ease-in-out': !isFixed,
+      'fixed top-0 left-0 w-full transition-all duration-500 ease-in-out': isFixed
+    }"
+  >
+    <nav class="flex justify-between items-center p-2 mx-4 bg-transparent">
+      <a href="/">
+        <img src="../assets/Img/logo_couleur-min.png" alt="Odysway's logo" class="w-32" />
       </a>
-      <img
-        src="../assets/Img/51ba6665-d97a-4173-ba49-c9557b013e45.svg"
-        alt="Menu"
-        class="w-6"
-        @click="displayMenu = !displayMenu"
-      />
-    </section>
-  </nav>
+      <section class="flex gap-6">
+        <img
+          src="../assets/Img/bf25558b-2a5d-43a2-9e75-2eff32be2ce8.svg"
+          alt="Searchbar"
+          class="w-6 hover:rounded-xl hover:bg-gray-100"
+          @click="displayModal = !displayModal"
+        />
+
+        <a href="/registration" class="w-6 hover:rounded-xl hover:bg-gray-100">
+          <img src="../assets/Img/048756c9-5947-4b56-8420-5d31d0957cfb.svg" alt="My account" />
+        </a>
+        <img
+          src="../assets/Img/51ba6665-d97a-4173-ba49-c9557b013e45.svg"
+          alt="Menu"
+          class="w-6"
+          @click="displayMenu = !displayMenu"
+        />
+      </section>
+    </nav>
+  </header>
 
   <section
     v-if="displayModal"
